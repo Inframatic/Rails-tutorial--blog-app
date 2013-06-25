cf62ff47376320aae7cff80d87dd7f84f6f8cf76
Inframatic <isteffy@hotmail.com>
2013-06-25 15:28:41 -0400

    using migrate'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '14cf550b4f4cc948353fb39277a2559575396a429b94cc6ef8b00e166cde3e190eb5af17bfa790c19fb7f2baf0197e292e2320bddc1e273233d27a4ccf4641f3'
